196a503c0eda99b5dbe5abf35097a4c0298203b7
YolloPlays <spexe0011@gmail.com>
2023-11-19 14:50:58 +0100

    necessary check before make_move to prevent loop

--- Game.py
+++ Game.py
@@ -5,14 +5,22 @@ class Game:
         self.board = board
         self.player1 = player1
         self.player2 = player2
+        self.playerturn = True
         self.gui = GUI.GUI(self)
         
     def game_move(self, m, n):
         # TODO handle real play and player change
-        print(m, n) # <= Reality do board manipulation
-        return True # <= needed for gui to now whos players turn it was True: player1, False: player 2
+        print(m, n) # <= Reality: player move
+        current_player_turn = self.playerturn
+        current_player = self.player1 if current_player_turn else self.player2
+        success = False
+        if self.board.array[m][n] == 0:
+            current_player.make_move(self.board, m, n)
+            success = True
+            self.playerturn = not self.playerturn
+        return (success, current_player_turn) # <= needed for gui to know whos players turn it was True: player1, False: player 2
         
         
         
 if __name__ == "__main__":
-    Game(Board.Board(), Player.Player("Klaus", 1), Player.Player("Peter", 1))
+    Game(Board.Board(), Player.Player("Klaus", 1), Player.Player("Peter", 2))
