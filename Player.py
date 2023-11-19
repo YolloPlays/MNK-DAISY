de110bd90db3c9443b22a236f6d97c5c31e309bf
OmegaRiot <84497435+OmegaRiot@users.noreply.github.com>
2023-11-19 12:26:03 +0100

    Player kann nun auch m und n angegeben bekommen

--- Player.py
+++ Player.py
@@ -5,10 +5,12 @@ class Player():
         self.player_number = player_number
 
     
-    def make_move(self, board):
+    def make_move(self, board, m = None, n = None):
         while True:
-            m = int(input(f"{self.name}, bitte geben Sie die Zeile an: ")) - 1
-            n = int(input(f"{self.name}, bitte geben Sie die Reihe an: ")) - 1
+            m = (int(input(f"{self.name}, bitte geben Sie die Zeile an: ")) - 1) \
+                if m is None else m - 1
+            n = (int(input(f"{self.name}, bitte geben Sie die Reihe an: ")) - 1) \
+                if n is None else n - 1
             if 0 <= m < board.m and 0 <= n < board.n:
                 if board.array[m][n] == 0:
                     board.array[m][n] = self.player_number
@@ -22,5 +24,5 @@ if __name__ == "__main__":
     # Aufruf der Methoden zum Spielen
     board = Board()
     player1 = Player("Klaus", 1)
-    player1.make_move(board)
+    player1.make_move(board, 1 ,2)
     board.display()
