cceb7ae14f5255744de615922e3436a26219f1d7
ricgoe <bluemchen_fenchel_0d@icloud.com>
2023-11-19 19:05:17 +0100

    added feature: - board now gives back who won - Player 1 = 1, Player 2 = 2, draw = 0 TODO: check if its necessary to check if board is full

--- Board.py
+++ Board.py
@@ -11,32 +11,45 @@ class Board:
         print(self.array)
         
     def has_won(self):
-        win = False
+        win = 0
         # Horizontal win
         for row in range(self.n):
             for col in range(self.m - self.k + 1):
-                if all(self.array[row][col + i] == self.array[row][col] and self.array[row][col] != 0 for i in range(1, self.k)):
-                    win = True
-
+                if all(self.array[row][col + i] == self.array[row][col] and self.array[row][col] == 1 for i in range(1, self.k)):
+                    win = 1
+                if all(self.array[row][col + i] == self.array[row][col] and self.array[row][col] == 2 for i in range(1, self.k)):
+                    win = 2
         # Check columns
         for col in range(self.m):
             for row in range(self.n - self.k + 1):
-                if all(self.array[row + i][col] == self.array[row][col] and self.array[row][col] != 0 for i in range(1, self.k)):
-                    win = True
+                if all(self.array[row + i][col] == self.array[row][col] and self.array[row][col] == 1 for i in range(1, self.k)):
+                    win = 1
+                if all(self.array[row + i][col] == self.array[row][col] and self.array[row][col] == 2 for i in range(1, self.k)):
+                    win = 2
         
         # Check diagonal
         for row in range(self.n - self.k + 1):
             for col in range(self.m - self.k + 1):
-                if all(self.array[row + i][col + i] == self.array[row][col] and self.array[row][col] != 0 for i in range(1, self.k)):
-                    win = True
+                if all(self.array[row + i][col + i] == self.array[row][col] and self.array[row][col] == 1 for i in range(1, self.k)):
+                    win = 1
+                if all(self.array[row + i][col + i] == self.array[row][col] and self.array[row][col] == 2 for i in range(1, self.k)):
+                    win = 2
 
         for row in range(self.n - self.k + 1):
             for col in range(self.k - 1, self.m):
-                if all(self.array[row + i][col - i] == self.array[row][col] and self.array[row][col] != 0 for i in range(1, self.k)):
-                    win = True
+                if all(self.array[row + i][col - i] == self.array[row][col] and self.array[row][col] == 1 for i in range(1, self.k)):
+                    win = 1
+                if all(self.array[row + i][col - i] == self.array[row][col] and self.array[row][col] == 2 for i in range(1, self.k)):
+                    win = 2
         
         return win
 
 if __name__ == "__main__":
     board = Board()
     board.display()
+    player = 2
+    board.array[0][0] = player
+    board.array[1][0] = player
+    board.array[2][0] = player
+    board.array[3][0] = player
+    print(board.has_won())
