e89dca09f35001a8a94c5983fc150930245feb58
OmegaRiot <84497435+OmegaRiot@users.noreply.github.com>
2023-11-19 12:02:19 +0100

    Player ist nun kein Board mehr :D

--- Player.py
+++ Player.py
@@ -1,5 +1,5 @@
 from Board import Board
-class Player(Board):
+class Player():
     def __init__(self, name: str, player_number: int):
         self.name = name
         self.player_number = player_number
