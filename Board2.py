1f68f92b9bd0041de26ea564cdef251cfadef12d
ricgoe <bluemchen_fenchel_0d@icloud.com>
2024-02-12 09:06:47 +0100

    - deleted redundant code

--- Board2.py
+++ Board2.py
@@ -133,21 +133,5 @@ if __name__ == "__main__":
     board.array[3, 3] = 2
     board.array[4, 4] = 2
 
-    
-    
-    # board.array[0, 1] = 2
-    # board.array[1, 1] = 1
-    # board.array[2, 1] = 1
-    # board.array[3, 1] = 2
-    
-    # board.array[0, 2] = 1
-    # board.array[1, 2] = 2
-    # board.array[2, 2] = 2
-    # board.array[3, 2] = 1
-    
-    # board.array[0, 3] = 2
-    # board.array[1, 3] = 1
-    # board.array[2, 3] = 1
-    # board.array[3, 3] = 2
     board.display()
     print(board.has_won())
